0dcf2d6bb4d32818dc080d6a0d523366854d88ac
nithin4testing <nithin4testing@gmail.com>
2025-11-17 16:43:55 +0530

    Update LoginTest.java
    modified

--- src/test/java/pack/LoginTest.java
+++ src/test/java/pack/LoginTest.java
@@ -3,8 +3,14 @@ package pack;
 import org.testng.annotations.Test;
 
 public class LoginTest {
+WebDriver Driver;
   @Test
   public void show() {
 	  System.out.println("Jenkins Introducing");
+  }
+  @Test
+  public void launchBrowser() {
+	  driver=new EdgeDriver();
+	  driver.get("https://demo.guru99.com/test/newtours/");
   }
 }
