af9d67b901ab9322cb3d9e7c69d5353815c3084f
nithin4testing <nithin4testing@gmail.com>
2025-11-17 16:50:56 +0530

    Update LoginTest.java

--- src/test/java/pack/LoginTest.java
+++ src/test/java/pack/LoginTest.java
@@ -8,7 +8,7 @@ public class LoginTest {
 WebDriver driver;
   @Test
   public void show() {
-	  System.out.println("Jenkins Introducing");
+	  System.out.println("Introducing Github");
   }
   @Test
   public void launchBrowser() {
